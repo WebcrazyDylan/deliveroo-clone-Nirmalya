b533a68eb20aa7c4ef71d612dd2beb3c1c83a44a
Nirmalya Nayak <n.nayak263@gmail.com>
2023-04-18 16:49:29 +0530

    added ResturantCard comp

--- components/FeaturedRow.jsx
+++ components/FeaturedRow.jsx
@@ -1,10 +1,64 @@
-import { View, Text } from "react-native";
+import { View, Text, ScrollView } from "react-native";
 import React from "react";
+import { ArrowRightIcon } from "react-native-heroicons/solid";
+import ResturantCard from "./ResturantCard";
 
 const FeaturedRow = ({ id, title, description }) => {
   return (
     <View>
-      <Text>FeaturedRow</Text>
+      <View className="mt-4 flex-row items-center justify-between px-4">
+        <Text className="font-bold text-lg">{title}</Text>
+        <ArrowRightIcon color="#00CCBB" />
+      </View>
+
+      <Text className="text-xs text-gray-500 px-4">{description}</Text>
+
+      <ScrollView
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={{
+          paddingHorizontal: 15,
+        }}
+        className="pt-4"
+      >
+        {/* ResturantCards */}
+        <ResturantCard
+          id={1}
+          imgUrl="https://links.papareact.com/wru"
+          title="text"
+          rating={4.5}
+          genre="Chineese"
+          address="Mukundapur"
+          short_description="short desc"
+          dishes={[]}
+          lon={1231232131}
+          lat={3142342344}
+        />
+        <ResturantCard
+          id={1}
+          imgUrl="https://links.papareact.com/wru"
+          title="text"
+          rating={4.5}
+          genre="Chineese"
+          address="Mukundapur"
+          short_description="short desc"
+          dishes={[]}
+          lon={1231232131}
+          lat={3142342344}
+        />
+        <ResturantCard
+          id={1}
+          imgUrl="https://links.papareact.com/wru"
+          title="text"
+          rating={4.5}
+          genre="Chineese"
+          address="Mukundapur"
+          short_description="short desc"
+          dishes={[]}
+          lon={1231232131}
+          lat={3142342344}
+        />
+      </ScrollView>
     </View>
   );
 };
