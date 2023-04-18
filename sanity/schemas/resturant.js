25bd93e4d793a1bb8a491e61f61c2e3257d6740d
Nirmalya Nayak <n.nayak263@gmail.com>
2023-04-18 17:23:43 +0530

    updated category schema

--- sanity/schemas/resturant.js
+++ sanity/schemas/resturant.js
@@ -5,55 +5,55 @@ export default defineType({
   title: 'Resturant',
   type: 'document',
   fields: [
-    {
+    defineField({
       name: 'name',
       type: 'string',
       title: 'Resturant Name',
       validation: (Rule) => Rule.required(),
-    },
-    {
+    }),
+    defineField({
       name: 'short_description',
       type: 'string',
       title: 'Short Description',
       validation: (Rule) => Rule.max(200),
-    },
-    {
+    }),
+    defineField({
       name: 'name',
       type: 'image',
       title: 'Image of Resturant',
-    },
-    {
+    }),
+    defineField({
       name: 'lat',
       type: 'number',
       title: 'Latiture of Resturant',
-    },
-    {
+    }),
+    defineField({
       name: 'lon',
       type: 'number',
       title: 'Longitude of Resturant',
-    },
-    {
+    }),
+    defineField({
       name: 'address',
       type: 'string',
       title: 'Resturant Address',
       validation: (Rule) => Rule.required(),
-    },
-    {
+    }),
+    defineField({
       name: 'rating',
       type: 'number',
       title: 'Enter a Rating from (1-5 Stars)',
       validation: (Rule) =>
         Rule.required().min(1).max(5).error('Please Enter a Value Between 1 to 5.'),
-    },
-    {
+    }),
+    defineField({
       name: 'type',
       type: 'string',
       title: 'Category',
       validation: (Rule) => Rule.required(),
       type: 'reference',
       to: [{type: 'category'}],
-    },
-    {
+    }),
+    defineField({
       name: 'dishes',
       type: 'array',
       title: 'Dishes',
@@ -67,6 +67,6 @@ export default defineType({
           ],
         },
       ],
-    },
+    }),
   ],
 })
