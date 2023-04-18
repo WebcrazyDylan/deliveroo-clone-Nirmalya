ff9bb5397c8d9be1346b8779c12197d116328826
Nirmalya Nayak <n.nayak263@gmail.com>
2023-04-18 11:59:13 +0530

    added  react-native-svg react-native-heroicons

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,10 +1,11 @@
-import { View, Text } from "react-native";
+import { View, Text, Image } from "react-native";
 import React, { useLayoutEffect } from "react";
 import { useNavigation } from "@react-navigation/native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function HomeScreen() {
   const navigation = useNavigation();
+
   useLayoutEffect(() => {
     navigation.setOptions({
       headerShown: false,
@@ -13,7 +14,23 @@ export default function HomeScreen() {
 
   return (
     <SafeAreaView>
-      <Text className="text-red-500">Home Screen</Text>
+      <Text className="text-red-500">
+        {/* HEADER */}
+        <View className="flex-row pb-3 items-center mx-4 space-x-2">
+          <Image
+            source={{
+              uri: "https://links.papareact.com/wru",
+            }}
+            className="h-7 w-7 bg-gray-300 p-4 rounded-full"
+          />
+          <View>
+            <Text className="font-bold text-gray-400 text-xs">
+              Deliver Now!
+            </Text>
+            <Text className="font-bold text-xl">Current Location</Text>
+          </View>
+        </View>
+      </Text>
     </SafeAreaView>
   );
 }
