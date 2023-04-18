0eb93237685102f75f455cc326de377c75c04a84
Nirmalya Nayak <n.nayak263@gmail.com>
2023-04-18 13:06:08 +0530

    created new comp CategoryCard

---
+++ components/CategoryCard.jsx
@@ -0,0 +1,13 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const CategoryCard = () => {
+  return (
+    <View>
+      {/* CategoryCard */}
+      <Text>CategoryCard</Text>
+    </View>
+  );
+};
+
+export default CategoryCard;
