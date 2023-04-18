da6b15a16756583aa6add94ecb689f92f35ec8be
Nirmalya Nayak <n.nayak263@gmail.com>
2023-04-18 11:48:50 +0530

    added react-native-safe-area-context

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,10 +1,19 @@
 import { View, Text } from "react-native";
-import React from "react";
+import React, { useLayoutEffect } from "react";
+import { useNavigation } from "@react-navigation/native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function HomeScreen() {
+  const navigation = useNavigation();
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  }, []);
+
   return (
-    <View>
-      <Text>HomeScreen</Text>
-    </View>
+    <SafeAreaView>
+      <Text className="text-red-500">Home Screen</Text>
+    </SafeAreaView>
   );
 }
