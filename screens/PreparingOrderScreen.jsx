78a6280bd1237c6f584aec25e1af931576fab47e
Nirmalya Nayak <n.nayak263@gmail.com>
2023-04-19 21:33:00 +0530

    added package react-native-animatable

--- screens/PreparingOrderScreen.jsx
+++ screens/PreparingOrderScreen.jsx
@@ -1,11 +1,12 @@
 import { View, Text } from "react-native";
 import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const PreparingOrderScreen = () => {
   return (
-    <View>
+    <SafeAreaView className="bg-[#00CCBB] flex-1 justify-center items-center">
       <Text>PreparingOrderScreen</Text>
-    </View>
+    </SafeAreaView>
   );
 };
 
