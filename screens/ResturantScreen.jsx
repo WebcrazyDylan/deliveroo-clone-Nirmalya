76459963b68c0b2dc7ce41bedaf7e4a3eb509b5b
Nirmalya Nayak <n.nayak263@gmail.com>
2023-04-19 12:30:16 +0530

    added back btn to goback

--- screens/ResturantScreen.jsx
+++ screens/ResturantScreen.jsx
@@ -1,11 +1,48 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Text, ScrollView, Image, TouchableOpacity } from "react-native";
+import React, { useLayoutEffect } from "react";
+import { useNavigation, useRoute } from "@react-navigation/native";
+import { urlFor } from "../sanity";
+import { ArrowLeftIcon } from "react-native-heroicons/solid";
 
 const ResturantScreen = () => {
+  navigation = useNavigation();
+
+  const {
+    params: {
+      id,
+      imgUrl,
+      title,
+      rating,
+      genre,
+      address,
+      short_description,
+      dishes,
+      lon,
+      lat,
+    },
+  } = useRoute();
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  }, []);
+
   return (
-    <View>
-      <Text>ResturantScreen</Text>
-    </View>
+    <ScrollView>
+      <View className="relative">
+        <Image
+          source={{ uri: urlFor(imgUrl).url() }}
+          className="w-full h-56 bg-gray-300 p-4"
+        />
+        <TouchableOpacity
+          onPress={navigation.goBack}
+          className="absolute top-10 left-5 p-2 bg-gray-100 rounded-full"
+        >
+          <ArrowLeftIcon color="#00CCBB" size={20} />
+        </TouchableOpacity>
+      </View>
+    </ScrollView>
   );
 };
 
