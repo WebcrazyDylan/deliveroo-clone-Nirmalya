5b88a64b49596b814163d156b9c02990df4a9931
Nirmalya Nayak <n.nayak263@gmail.com>
2023-04-19 22:02:32 +0530

    show dummy progress bar in delivery screen

--- screens/DeliveryScreen.jsx
+++ screens/DeliveryScreen.jsx
@@ -1,10 +1,11 @@
-import { View, Text, TouchableOpacity } from "react-native";
+import { View, Text, TouchableOpacity, Image } from "react-native";
 import React from "react";
 import { useNavigation } from "@react-navigation/native";
 import { useSelector } from "react-redux";
 import { selectResturant } from "../features/resturantSlice";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { XCircleIcon } from "react-native-heroicons/solid";
+import * as Progress from "react-native-progress";
 
 const DeliveryScreen = () => {
   const navigation = useNavigation();
@@ -21,10 +22,21 @@ const DeliveryScreen = () => {
         </View>
 
         <View className="bg-white mx-5 my-2 rounded-md p-6 z-50 shadow-md">
-          <View>
-            <Text className="text-lg text-gray-400">Estimated Arrival</Text>
-            <Text className="text-4xl font-bold">45-55 Minutes</Text>
+          <View className="flex-row justify-between">
+            <View>
+              <Text className="text-lg text-gray-400">Estimated Arrival</Text>
+              <Text className="text-4xl font-bold">45-55 Minutes</Text>
+            </View>
+
+            <Image
+              source={{
+                uri: "https://links.papareact.com/fls",
+              }}
+              className="h-20 w-20"
+            />
           </View>
+
+          <Progress.Bar size={30} color="#00CCBB" indeterminate={true} />
         </View>
       </SafeAreaView>
     </View>
