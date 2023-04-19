2664e3ed05a055968f633051c0ffea84316b2674
Nirmalya Nayak <n.nayak263@gmail.com>
2023-04-19 21:39:13 +0530

    added animatable img

--- screens/PreparingOrderScreen.jsx
+++ screens/PreparingOrderScreen.jsx
@@ -5,7 +5,14 @@ import * as Animatable from "react-native-animatable";
 
 const PreparingOrderScreen = () => {
   return (
-    <SafeAreaView className="bg-[#00CCBB] flex-1 justify-center items-center"></SafeAreaView>
+    <SafeAreaView className="bg-[#00CCBB] flex-1 justify-center items-center">
+      <Animatable.Image
+        source={require("../assets/prepare.gif")}
+        animation="slideInUp"
+        iterationCount={1}
+        className="h-96 w-96"
+      />
+    </SafeAreaView>
   );
 };
 
