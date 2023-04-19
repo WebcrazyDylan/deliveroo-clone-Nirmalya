02f6c467fa26e4e386c45a394456206be8ee50c1
Nirmalya Nayak <n.nayak263@gmail.com>
2023-04-19 21:36:35 +0530

    added assets

--- screens/PreparingOrderScreen.jsx
+++ screens/PreparingOrderScreen.jsx
@@ -1,12 +1,11 @@
 import { View, Text } from "react-native";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
+import * as Animatable from "react-native-animatable";
 
 const PreparingOrderScreen = () => {
   return (
-    <SafeAreaView className="bg-[#00CCBB] flex-1 justify-center items-center">
-      <Text>PreparingOrderScreen</Text>
-    </SafeAreaView>
+    <SafeAreaView className="bg-[#00CCBB] flex-1 justify-center items-center"></SafeAreaView>
   );
 };
 
