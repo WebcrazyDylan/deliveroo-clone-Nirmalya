c9f24a1d03d7c580e9e05c58e8a78df2723361b5
Nirmalya Nayak <n.nayak263@gmail.com>
2023-04-19 21:59:23 +0530

    show dummy txt in delivery screen

--- screens/DeliveryScreen.jsx
+++ screens/DeliveryScreen.jsx
@@ -1,10 +1,32 @@
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity } from "react-native";
 import React from "react";
+import { useNavigation } from "@react-navigation/native";
+import { useSelector } from "react-redux";
+import { selectResturant } from "../features/resturantSlice";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { XCircleIcon } from "react-native-heroicons/solid";
 
 const DeliveryScreen = () => {
+  const navigation = useNavigation();
+  const resturant = useSelector(selectResturant);
+
   return (
-    <View>
-      <Text>DeliveryScreen</Text>
+    <View className="bg-[#00CCBB] flex-1">
+      <SafeAreaView className="z-50">
+        <View className="flex-row justify-between items-center p-5">
+          <TouchableOpacity onPress={() => navigation.navigate("Home")}>
+            <XCircleIcon color="white" size={30} />
+          </TouchableOpacity>
+          <Text className="text-white font-light text-lg">Order Help</Text>
+        </View>
+
+        <View className="bg-white mx-5 my-2 rounded-md p-6 z-50 shadow-md">
+          <View>
+            <Text className="text-lg text-gray-400">Estimated Arrival</Text>
+            <Text className="text-4xl font-bold">45-55 Minutes</Text>
+          </View>
+        </View>
+      </SafeAreaView>
     </View>
   );
 };
