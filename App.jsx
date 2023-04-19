30a32d79e43cd8fb9cf830c666fd32218413fe35
Nirmalya Nayak <n.nayak263@gmail.com>
2023-04-19 14:34:01 +0530

    added basket slice

--- App.jsx
+++ App.jsx
@@ -5,15 +5,20 @@ import "react-native-url-polyfill/auto";
 import HomeScreen from "./screens/HomeScreen";
 import ResturantScreen from "./screens/ResturantScreen";
 
+import { store } from "./store";
+import { Provider } from "react-redux";
+
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Resturant" component={ResturantScreen} />
-      </Stack.Navigator>
+      <Provider store={store}>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="Resturant" component={ResturantScreen} />
+        </Stack.Navigator>
+      </Provider>
     </NavigationContainer>
   );
 }
